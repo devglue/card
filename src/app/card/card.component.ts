a621ae1e7735eb1e238c7ec17fe98a8d51a5312b
dreamerkumar <connectwithvkumar@gmail.com>
2019-02-24 18:17:26 -0500

    use scss instead of less

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'devglue-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.less']
+  styleUrls: ['./card.component.scss']
 })
 export class CardComponent implements OnInit {
 
