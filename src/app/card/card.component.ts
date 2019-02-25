e8a99697f2b238c18cd54bcc61f66864728e7f8b
dreamerkumar <connectwithvkumar@gmail.com>
2019-02-24 21:56:23 -0500

    implemented the card component with proper look and feel controlling the font colors on card through yml files rrethinking how the dynamic scss will be generated and imported

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
 
 @Component({
-  selector: 'devglue-card',
-  templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+  selector: "devglue-card",
+  templateUrl: "./card.component.html",
+  styleUrls: ["./card.component.scss"]
 })
 export class CardComponent implements OnInit {
+  @Input() title: string;
+  @Input() src: string;
+  @Input() alt: string;
+  @Input() description: string;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
